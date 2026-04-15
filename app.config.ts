0700cef7a01b47f324d76d6e485141c22b63175a
Tanju Baştürk <basturktanju@gmail.com>
2026-04-15 23:25:17 +0300

    chore: build 8

--- app.config.ts
+++ app.config.ts
@@ -50,7 +50,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "7",
+    buildNumber: "8",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
